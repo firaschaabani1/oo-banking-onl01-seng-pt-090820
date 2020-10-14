7a9a482746d4cb461c0906ccf0aa05f4d4aa96df
Firas Chaabani <firaschaabani1@gmail.com>
2020-10-14 01:52:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,6 +7,31 @@ class Transfer
     @amount = amount
     @status = "pending"
   end
-  
+   def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if self.status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      self.status = "complete"
+    end
+    if !self.valid?
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      self.status = "reversed"
+    end
+  end
+
+
+
 
 end
